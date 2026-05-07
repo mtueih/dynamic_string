34a4206cef1621b144479d35a67113330c92984b
mtueih <mtueih.org@outlook.com>
2026-05-07 22:29:19 +0800

    2026-05-07 22:29:19

--- src/dynamic_string.c
+++ src/dynamic_string.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <safe_calc.h>
 
 #define DSTR_CACHELINE_SIZE 64
 
@@ -23,67 +24,114 @@ struct dynamic_string {
 // 静态函数定义
 // 计算对齐值
 static size_t align_up(const size_t base, const size_t align) {
+	size_t remainder;
+
+	// 防止除 0
 	if (align == 0) return base;
 
+	// 如果 align 是 2 的幂，则使用更高效的计算方法
 	if ((align & (align - 1)) == 0) {
-		return (base & (align - 1)) == 0 ? base : (base + align - 1) & ~(align - 1);
+		return (base & (align - 1)) == 0 ? base : ((base + align - 1) & ~(align - 1));
 	}
+
+	// 常规计算方法
+	// 如果 base 已经是 align 的倍数，则直接返回
+	remainder = base % align;
+	if (remainder == 0) return base;
 
 	return base % align == 0 ? base : (base + align - 1) / align * align;
 }
 
 // 动态调整一个「动态字符串」的容量
-static bool capacity_resize_dynamic(dstr_adt *const dstr, const size_t new_len) {
-	char *new_cstr;
-	size_t new_cap;
-	size_t adjusted_cap;
+static int capacity_resize_dynamic(dstr_adt *const dstr, const size_t new_len) {
+	char *new_cstr = NULL;
+	size_t new_cap = 0; // 不低于保底值目标容量
+	size_t adjusted_cap = 0; // 基于目标容量调整后的容量，为目标容量 1.5 倍
+	size_t cache_line_aligned_cap = 0; // 对齐到缓存行大小后的容量
 
-	new_cap = new_len + 1;
-
-	new_cap = new_cap > dstr->min_cap
-			  ? new_cap
-			  : dstr->min_cap;
-
-	// new_cap 为 1，表示字符串长度为空，不必单独存储 '\0'，释放内存
-	if (new_cap == 1) {
-		if (dstr->data != NULL) free(dstr->data);
-		*dstr = (dstr_adt){0};
-		return true;
+	// 安全计算 size_t 加法，防止溢出
+	// new_cap = new_len + 1;
+	if (!safe_size_add(new_len, 1, &new_cap)) {
+		return DSTR_OVERFLOW;
 	}
 
-	// 如果新容量与当前容量相同，则直接返回 true
+	// 确保容量不低于保底值
+	if (new_cap < dstr->min_cap) {
+		new_cap = dstr->min_cap;
+	}
+
+	// 如果目标容量与当前容量相同，则直接返回
 	if (new_cap == dstr->cap) {
-		return true;
+		return DSTR_SUCCESS;
 	}
 
-	// 如果新容量小于当前容量，则延迟减容，仅当新容量小于当前容量的 1/4 时，才减容
+	// 如果目标容量小于当前容量，则延迟减容，仅当新容量小于当前容量的 1/4 时，才减容
+	// 即，如果目标容量小于当前容量，但是又大于当前容量的 1/4，则不进行减容
+	// 右移操作无溢出风险
 	if (new_cap < dstr->cap && new_cap > dstr->cap >> 2) {
-		return true;
+		return DSTR_SUCCESS;
 	}
 
-	// 计算与缓存行对齐的容量
-	adjusted_cap = align_up(new_cap, DSTR_CACHELINE_SIZE);
+	// 如果目标容量大于当前容量，则进行容量预分配，分配 1.5 倍
+	if (new_cap > dstr->cap) {
+		// 尝试预分配内存
+		// 安全计算 size_t 加法，防止溢出
+		// adjusted_cap = new_cap + new_cap >> 1;
+		safe_size_add(new_cap, new_cap >> 1, &adjusted_cap);
+	}
 
-	// 分配向上对齐到缓存行的整数倍的容量
-	new_cstr = (char *) realloc(dstr->data, adjusted_cap);
-	if (new_cstr == NULL) goto memory_allocation_successful;
 
-	if (adjusted_cap <= new_cap) return false;
+	// 如果 adjusted_cap > new_cap，则基于 adjusted_cap 对齐
+	if (adjusted_cap > new_cap) {
+		// 计算与缓存行对齐的容量
+		// 安全执行向上对齐，防止溢出
+		safe_size_align_up(adjusted_cap, DSTR_CACHELINE_SIZE, &cache_line_aligned_cap);
+	} else {
+		// 否则基于 new_cap 对齐
+		// 计算与缓存行对齐的容量
+		// 安全执行向上对齐，防止溢出
+		safe_size_align_up(new_cap, DSTR_CACHELINE_SIZE, &cache_line_aligned_cap);
+	}
 
-	// 分配向上对齐到指针大小的整数倍的容量
-	if (sizeof(void *) < DSTR_CACHELINE_SIZE) {
-		adjusted_cap = align_up(new_cap, sizeof(void *));
 
+	// 如果 cache_line_aligned_cap > new_cap，则说明基于 adjusted_cap 或 new_cap 的对齐值计算成功
+	// 此时尝试分配 cache_line_aligned_cap 容量的内存
+	if (cache_line_aligned_cap > new_cap) {
+		new_cstr = (char *) realloc(dstr->data, cache_line_aligned_cap);
+	} else {
+		// 否则说明不管是基于 adjusted_cap 对齐，
+		// 还是基于 new_cap 的对齐，
+		// 都失败了，此时一定回退到直接分配 new_cap 容量的内存
+		new_cstr = (char *) realloc(dstr->data, new_cap);
+	}
+
+	// 如果分配成功，则跳转到收尾工作
+	if (new_cstr) goto memory_allocation_successful;
+	// 如果失败，有两种可能：
+	// 1. 一种是分配 new_cap 容量的内存失败
+	//	此时毫无悬念内存分配失败了
+	// 2. 另一种可能是分配 cache_line_aligned_cap 容量的内存失败
+	//	此时也有两种可能，一种是基于 adjusted_cap 的对齐值，
+	//		另一种则是基于 new_cap 的对齐值
+	//	如果是前者，则说明 adjusted_cap 计算成功了，而基于它的对齐值分配失败
+	//	那就尝试回退到 adjusted_cap 本身，
+	//	但如果 cache_line_aligned_cap 不比 adjusted_cap 大，那就没有尝试的必要了
+	if (adjusted_cap > new_cap && cache_line_aligned_cap > adjusted_cap) {
 		new_cstr = (char *) realloc(dstr->data, adjusted_cap);
-		if (new_cstr != NULL) goto memory_allocation_successful;
-
-		if (adjusted_cap <= new_cap) return false;
+	} else if (cache_line_aligned_cap > new_cap) {
+		// 否则，后者，说明 adjusted_cap 计算失败，
+		// cache_line_aligned_cap 是基于 new_cap 的，
+		// 那就尝试回退到 new_cap 本身，
+		// 同样，如果 cache_line_aligned_cap 不比 new_cap 大，那就没有尝试的必要了
+		new_cstr = (char *) realloc(dstr->data, new_cap);
+	} else {
+		return DSTR_MEMORY_ALLOC_FAILED;
 	}
 
-	// 不进行预分配
-	adjusted_cap = new_cap;
-	new_cstr = (char *) realloc(dstr->data, adjusted_cap);
-	if (new_cstr == NULL) return false;
+	// 如果分配成功，则跳转到收尾工作
+	if (new_cstr) goto memory_allocation_successful;
+
+
 
 memory_allocation_successful:
 	dstr->data = new_cstr;
@@ -106,6 +154,12 @@ static bool capacity_resize(dstr_adt *const dstr, const size_t new_cap) {
 
 	dstr->data = new_cstr;
 	dstr->cap = new_cap;
+
+	if (new_cap <= dstr->len) {
+		dstr->len -= dstr->len + 1 - new_cap;
+		dstr->data[dstr->len] = '\0';
+	}
+
 	return true;
 }
 
@@ -335,12 +389,12 @@ int dstr_cat_sub_cstr(dstr_adt *const dest, const char *const src, const size_t 
 
 	sub_len = sub_count == 0 ? src_len - sub_index : sub_count;
 
-	if (!capacity_resize_dynamic(dest, dest->len + sub_count)) {
+	if (!capacity_resize_dynamic(dest, dest->len + sub_len)) {
 		return DSTR_MEMORY_ALLOC_FAILED;
 	}
 
 	memcpy(dest->data + dest->len, src + sub_index, sub_len);
-	dest->data[dest->len += src_len] = '\0';
+	dest->data[dest->len += sub_len] = '\0';
 	return DSTR_SUCCESS;
 }
 
@@ -454,7 +508,7 @@ void dstr_trim(dstr_adt *const dstr, const char *const trim_chars) {
 		if (trim_chars == NULL || strlen(trim_chars) == 0) {
 			if (!isspace(*find)) break;
 		} else {
-			if (strchr(trim_chars, *find)) break;
+			if (strchr(trim_chars, *find) == NULL) break;
 		}
 	}
 
@@ -815,7 +869,8 @@ int dstr_replace_cstr(dstr_adt *const dstr, const char *const old_str, const cha
 	size_t old_count;
 
 	// 参数检查
-	assert(dstr != NULL && old != NULL);
+	assert(dstr != NULL);
+	assert(old_str != NULL);
 
 	old_len = strlen(old_str);
 	if (old_len == 0 || old_len > dstr->len) return DSTR_INVALID_PARAM;
@@ -906,7 +961,8 @@ int dstr_replace(dstr_adt *const dstr, const dstr_adt *const old_str, const dstr
 
 
 	// 参数检查
-	assert(dstr != NULL && old != NULL);
+	assert(dstr != NULL);
+	assert(old_str != NULL);
 
 	if (old_str->len == 0 || old_str->len > dstr->len) return DSTR_INVALID_PARAM;
 
@@ -954,9 +1010,9 @@ int dstr_replace(dstr_adt *const dstr, const dstr_adt *const old_str, const dstr
 	} else {
 		for (find_count = 0, find = dstr->data;
 		     find <= dstr->data + dstr->len - old_str->len && (n == 0
-									   ? true
-									   : find_count < n
-									     && find_count < old_count);
+									       ? true
+									       : find_count < n
+										       && find_count < old_count);
 		) {
 			if (strncmp(find, old_str->data, old_str->len) == 0) {
 				++find_count;
